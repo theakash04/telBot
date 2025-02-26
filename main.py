de3b30ab445dac43b205c530464094a3b037a3fd
theakash04 <aakashkkr04@gmail.com>
2025-02-26 07:01:11 +0530

    update

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def get_manga_list():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
         "Accept": "application/json",
+        "Referer": "https://comick.io",
     }
     try:
         response = requests.get(url, headers=headers)
